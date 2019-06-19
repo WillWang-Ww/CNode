4245448f939e84fbcfefba2d49bb6824257089be
WILLWANG <wangziweichn@outlook.com>
2019-06-19 23:36:40 +0800

    UserInfo基本CSS完成

--- src/components/UserInfo.vue
+++ src/components/UserInfo.vue
@@ -2,23 +2,37 @@
   <div class="UserInfo">
     <div class="userMessage">
       <div class="mainList">
-        <div class="backMainList">
-          <router-link :to="{name:'root'}"><span>主页</span></router-link>
+        <div class="backMainList topNav">
+          <router-link :to="{name:'root'}" class="aLink"><div class="mainListLink">主页</div></router-link>
         </div>
         <div class="info">
           <img :src="userinfo.avatar_url" alt="">
           <span>{{userinfo.loginname}}</span>
           <p>{{userinfo.score}} 积分</p>
           <p>注册时间 {{userinfo.create_at | timeFilter}}</p>
-          <a href="`https://github.com/${userinfo.githubUsername}`">{{userinfo.githubUsername}}</a>
+          <a href="`https://github.com/${userinfo.githubUsername}`" class="aLink listStyle">{{userinfo.githubUsername}}</a>
         </div>
       </div>
     </div>
     <div class="creatTopic">
-
+      <div class="topNav">最近创建的话题</div>
+      <ul>
+        <li v-for="items in userinfo.recent_topics" class="listStyle postCard">
+          <router-link :to="{name:'post_content',params:{id:items.id}}" class="aLink">
+            <p class="title listStyle">{{items.title}}</p>
+          </router-link>
+        </li>
+      </ul>
     </div>
     <div class="joinTopic">
-
+      <div class="topNav">最近参与的话题</div>
+      <ul>
+        <li v-for="items in userinfo.recent_replies" class="listStyle postCard">
+          <router-link :to="{name:'post_content',params:{id:items.id}}" class="aLink">
+            <p class="title">{{items.title}}</p>
+          </router-link>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
@@ -28,7 +42,7 @@
     name: 'UserInfo',
     data () {
       return {
-        userinfo:{}
+        userinfo: {}
       }
     },
     methods: {
@@ -48,5 +62,63 @@
   }
 </script>
 <style scoped>
-
+  .userMessage {
+      border-radius: 5px;
+      background-color: white;
+      margin-top: 10px;
+      margin-left: 50px;
+      margin-right: 50px;
+    }
+  .creatTopic{
+    border-radius: 5px;
+    background-color: white;
+    margin-top: 10px;
+    margin-left: 50px;
+    margin-right: 50px;
+  }
+  .joinTopic{
+    border-radius: 5px;
+    background-color: white;
+    margin-top: 10px;
+    margin-left: 50px;
+    margin-right: 50px;
+  }
+  .topNav{
+    color: #444444;
+    background-color: #f6f6f6;
+    padding: 10px;
+  }
+  .mainListLink{
+    font-size: 14px;
+    color: #80bd01;
+  }
+  .aLink{
+    text-decoration: none;
+  }
+  .info > img {
+    height: 40px;
+    width: 40px;
+  }
+  .info{
+    padding: 10px;
+  }
+  .info > span,.info > p{
+    color: #778087;
+    font-size: 14px;
+    padding-left: 5px;
+  }
+  .listStyle{
+    list-style: none;
+  }
+  .postCard {
+    background-color: #ffffff;
+    display: flex;
+    align-items: center;
+    vertical-align: top;
+    border-top: 1px solid #f6f6f6;
+  }
+  ul{
+    margin: 0;
+    padding:0px 25px;
+  }
 </style>
